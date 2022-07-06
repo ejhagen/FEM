0c61f8e6ebf526db9e553ce579aaf3cad165a062
Eric Hagen <ericjameshagen@gmail.com>
2022-07-06 16:22:47 -0600

    configured eslint for react jsx

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,30 +1,37 @@
-const Pet = (props) => {
-  return React.createElement("div", null, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Apopt Me!"),
-    React.createElement(Pet, {
-      name: "Owsley",
-      animal: "Dog",
-      breed: "Border Collie",
-    }),
-    React.createElement(Pet, {
-      name: "Kemo",
-      animal: "Dog",
-      breed: "American Eskimo",
-    }),
-    React.createElement(Pet, {
-      name: "Bailey",
-      animal: "Dog",
-      breed: "Beagle",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Owsley" animal="dog" breed="Border Collie" />
+      <Pet name="Kemo" animal="dog" breed="American Eskimo" />
+      <Pet name="Bailey" animal="dog" breed="Beagle" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Apopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Owsley",
+//       animal: "Dog",
+//       breed: "Border Collie",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Kemo",
+//       animal: "Dog",
+//       breed: "American Eskimo",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Bailey",
+//       animal: "Dog",
+//       breed: "Beagle",
+//     }),
+//   ]);
+// };
